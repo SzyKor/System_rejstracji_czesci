1c0284232ee4a4d8b31e77116973b84533176bb6
Szymon <szymon.korpusinski@gmail.com>
2021-11-26 14:37:23 +0100

    Adding base files

---
+++ src/main/java/pl/edu/pbs/system_rejstracji_czesci/SystemRejstracjiCzesciApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pbs.system_rejstracji_czesci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemRejstracjiCzesciApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemRejstracjiCzesciApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/pbs/system_rejstracji_czesci/SystemRejstracjiCzesciApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pbs.system_rejstracji_czesci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemRejstracjiCzesciApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
