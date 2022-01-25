095eba14b34bbcd4400cd73fe1c522f5b524912a
Fenixx74540 <mateuszlassa@gmail.com>
2022-01-26 00:57:52 +0100

    Modyfikacja POM i dodanie Vaadin Designer

---
+++ src/main/java/pl/edu/pbs/system_rejstracji_czesci/MainUi.java
@@ -0,0 +1,24 @@
+package pl.edu.pbs.system_rejstracji_czesci;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+
+/**
+ * A Designer generated component for the main-ui template.
+ *
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("main-ui")
+@JsModule("./main-ui.ts")
+public class MainUi extends LitTemplate {
+
+    /**
+     * Creates a new MainUi.
+     */
+    public MainUi() {
+        // You can initialise any data required for the connected UI components here.
+    }
+
+}
--- src/main/java/pl/edu/pbs/system_rejstracji_czesci/ui/MainUI.java
+++
@@ -1,4 +0,0 @@
-package pl.edu.pbs.system_rejstracji_czesci.ui;
-
-public class MainUI {
-}
